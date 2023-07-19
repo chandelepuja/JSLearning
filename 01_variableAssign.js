641002016ba6029319c81b3d5dd9fa9286e0c6a4
chandelepuja <chandelepuja1@gmail.com>
2023-07-19 23:36:23 +0530

    use variable, data type and typeof operator
    use variable, data type and typeof operator

---
+++ 01_variableAssign.js
@@ -0,0 +1,32 @@
+var myFullName;
+console.log(myFullName);
+
+var myFullName ="Pooja Chandele";
+console.log(myFullName);
+
+var myFullName = "Pooja Gopal Chandele";
+console.log(myFullName);
+
+var myLove = "I love only JS";
+console.log(myLove);
+
+var  firstName = "First Name: Pooja";
+console.log(firstName);
+
+var lastName = "Last Name: Chandele";
+console.log(lastName);
+
+var age = "Age: 25";
+console.log(age);
+
+var address = "Address: Pune";
+console.log(address);
+
+var pinCode = "Pin Code: 411036";
+console.log(pinCode);
+
+var gender = "Gender: Female";
+console.log(gender);
+
+var isMarried = "Is Married: True";
+console.log(isMarried);
---
+++ 02_typeOf.js
@@ -0,0 +1,25 @@
+var bankName;
+console.log(bankName);
+
+var bankName = "Bank Name Is: SBI Bank";
+console.log(bankName);
+console.log(typeof bankName);
+
+var marks = "90%";
+console.log(marks);
+console.log(typeof marks);
+
+var isWorking;
+console.log(isWorking);
+
+var isWorking = false;
+console.log(isWorking);
+console.log(typeof isWorking);
+
+var totalCount = "Hundred and Seven";
+console.log(totalCount);
+console.log(typeof totalCount);
+
+var totalCount = 107;
+console.log(totalCount);
+console.log(typeof totalCount);
