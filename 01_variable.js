260a213d4b6bab8115447f57457f99857ce58ac5
chandelepuja <chandelepuja1@gmail.com>
2023-07-19 07:51:00 +0530

    variable
    variable

---
+++ 01_variable.js
@@ -0,0 +1,12 @@
+
+
+var myName; // Variable Declaration
+myName = "Pooja"; // Variable Assignment or Initialization
+
+console.log(myName);
+
+var city = "Pune"; //  Variable Assignment or Initialization
+console.log(city);
+
+var pin; //  Variable Assignment 
+console.log(pin);
